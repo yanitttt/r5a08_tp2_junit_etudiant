ae1d870eef2e5b87d6190bb6f67c4d5210cea8d6
bl <bouchkhaf@gmail.com>
2025-11-19 15:26:48 +0100

    step 2

---
+++ src/test/java/TextBufferTest.java
@@ -0,0 +1,19 @@
+import fr.einfolearning.tp2.metiers.TextBuffer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TextBufferTest {
+
+    @Test
+    public void should_return_sb(){
+        // Arrangee
+        TextBuffer textBuffer = new TextBuffer("je suis cool");
+        String expected_sb = "je suis cool";
+
+        //Act
+        String sb = textBuffer.toString();
+
+        //Assert
+        Assertions.assertEquals(expected_sb,sb);
+    }
+}
